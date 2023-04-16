88015be5b3bf6bc3fabb8a7df168f281c4bb38f9
sholabioku <bilushonline@gmail.com>
2023-04-16 09:35:40 +0100

    Update frontend url

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://pinvent-app.vercel.app'],
+    origin: ['http://localhost:3000', 'https://pinvent-app-chi.vercel.app'],
     credentials: true,
   })
 );
